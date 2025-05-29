ed3040e394608e6a0ff3b30b49ffafadb4cda9c4
apuri92 <akshat.puri92@gmail.com>
2025-05-28 21:32:23 -0700

    added current date

--- myAssistant/agent.py
+++ myAssistant/agent.py
@@ -4,6 +4,8 @@ from google.adk.agents import Agent
 from google.adk.sessions import InMemorySessionService
 from google.adk.runners import Runner
 from google.genai import types # For creating message Content/Parts
+from datetime import datetime
+import tzlocal
 from .tools import create_calendar_event
 
 import warnings
@@ -20,8 +22,7 @@ root_agent = Agent(
     name="weather_time_agent",
     model="gemini-2.0-flash",
     description="A helpful AI Assistant",
-    instruction="You are a helpful agent who can answer user questions and create calendar events." \
-    "When using the create_calendar_event tool, make sure to use ISO 8601 format and IANA timezone names. also return to the user a link to the calendar event that they can click on. this is available under the response: htmlLink key in the tool response. Before you invoke the tool, if the user is adding a flight, ask for the booking id and use that for the description.",
+    instruction=f"""You are a helpful agent who can answer user questions and create calendar events.
+    When using the create_calendar_event tool, make sure to use ISO 8601 format and IANA timezone names. also return to the user a link to the calendar event that they can click on. this is available under the response: htmlLink key in the tool response. Before you invoke the tool, if the user is adding a flight, ask for the booking id and use that for the description. Todays date is {datetime.now(tzlocal.get_localzone()).date()} and we are in the timezone {tzlocal.get_localzone()}.""",
     tools=[create_calendar_event],
 )
-
